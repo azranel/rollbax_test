75e8ee628e5aff17ca9167fd46797c64b5ff11b4
azranel <chunkyo@gmail.com>
2017-05-22 22:22:44 +0200

    Change to more advanced Rollbar integration
    This way we can inform Rollbar more about what browser, OS client used
    and structure our data better.

--- web/router.ex
+++ web/router.ex
@@ -26,6 +26,39 @@ defmodule RollbarExample.Router do
   # end
 
   defp handle_errors(conn, %{kind: kind, reason: reason, stack: stacktrace}) do
-    Rollbax.report(kind, reason, stacktrace)
+    # First, we filter params so we won't send any sensitive data
+    conn =
+      conn
+      |> Plug.Conn.fetch_cookies()
+      |> Plug.Conn.fetch_query_params()
+
+    params =
+      for {key, _value} = tuple <- conn.params do
+        if key in ["password", "password_confirmation"] do
+          {key, "[FILTERED]"}
+        else
+          tuple
+        end
+      end
+
+    # We make use of Rollbar Item POST API
+    # More info about it here: https://rollbar.com/docs/api/items_post/
+    conn_data = %{
+      "request" => %{
+        "cookies" => conn.req_cookies,
+        "url" => "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
+        "user_ip" => (conn.remote_ip |> Tuple.to_list() |> Enum.join(".")),
+        "headers" => Enum.into(conn.req_headers, %{}),
+        "params" => params,
+        "method" => conn.method,
+      },
+      "server" => %{
+        "pid" => System.get_env("MY_SERVER_PID"),
+        "host" => "#{System.get_env("MY_HOSTNAME")}:#{System.get_env("MY_PORT")}",
+        "root" => System.get_env("MY_APPLICATION_PATH"),
+      },
+    }
+
+    Rollbax.report(kind, reason, stacktrace, %{}, conn_data)
   end
 end
